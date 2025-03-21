cca40fffe4fdcd96c008a4cda5fda5a8b985f700
ahmadjafar14 <ahmadjafarshodiq14@gmail.com>
2025-03-21 17:27:42 +0700

    tambah api

--- lib/methods/api.dart
+++ lib/methods/api.dart
@@ -3,7 +3,8 @@ import 'package:http/http.dart' as http;
 import 'package:sibadeanmob_v2/helper/constant.dart';
 
 class API {
-  postRequest({
+  // Method POST untuk request ke API
+  Future<http.Response> postRequest({
     required String route,
     required Map<String, String> data,
   }) async {
@@ -15,13 +16,64 @@ class API {
         headers: _header(),
       );
     } catch (e) {
-      print(e.toString());
-      return jsonEncode(e);
+      print("Error: ${e.toString()}");
+      return http.Response(jsonEncode({'error': e.toString()}), 500);
     }
   }
 
-  _header() => {
-        'Content-type': 'application/json',
-        'Accept': 'application/json',
-      };
+  // Login User
+  Future<http.Response> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    Map<String, String> data = {
+      "email": email,
+      "password": password,
+    };
+
+    return await postRequest(route: "/login", data: data);
+  }
+
+  // Register User
+  Future<http.Response> registerUser({
+    required String fullName,
+    required String nik,
+    required String noKk,
+    required String tempatLahir,
+    required String tanggalLahir,
+    required String jenisKelamin,
+    required String alamat,
+    required String pekerjaan,
+    required String agama,
+    required String phone,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    Map<String, String> data = {
+      "nama_lengkap": fullName,
+      "nik": nik,
+      "no_kk": noKk,
+      "tempat_lahir": tempatLahir,
+      "tanggal_lahir": tanggalLahir,
+      "jenis_kelamin": jenisKelamin,
+      "alamat": alamat,
+      "pekerjaan": pekerjaan,
+      "agama": agama,
+      "no_hp": phone,
+      "email": email,
+      "password": password,
+      "password_confirmation": confirmPassword,
+    };
+
+    return await postRequest(route: "/register", data: data);
+  }
+
+  // Header untuk request ke API
+  Map<String, String> _header() {
+    return {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    };
+  }
 }
--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -1,14 +1,10 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sibadeanmob_v2/methods/api.dart';
-import 'package:sibadeanmob_v2/providers/auth_provider.dart';
 import 'package:sibadeanmob_v2/theme/theme.dart';
 import 'package:sibadeanmob_v2/views/auth/verifikasi.dart';
 import 'package:sibadeanmob_v2/views/dashboard_comunity/dashboard/dashboard_warga.dart';
-import 'package:sibadeanmob_v2/widgets/costum_button.dart';
 import 'package:sibadeanmob_v2/widgets/costum_scaffold1.dart';
 import 'package:sibadeanmob_v2/widgets/costum_texfield.dart';
 
@@ -36,60 +32,49 @@ class _LoginState extends State<Login> {
 
   void loginUser() async {
     final data = {
-      'nik': nikController.text.toString(),
-      'password': passwordController.text.toString(),
+      'nik': nikController.text.trim(),
+      'password': passwordController.text.trim(),
     };
-    final result = await API().postRequest(route: '/login', data: data);
-    final response = jsonDecode(result.body);
-    if (response['status'] == 200) {
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      await preferences.setInt('user_id', response['user']['id']);
-      await preferences.setString('nik', response['user']['nik']);
-      await preferences.setString('password', response['user']['password']);
-      await preferences.setString('token', response['token']);
+
+    print("Mengirim request login dengan data: $data");
+
+    try {
+      final response = await API().postRequest(route: '/login', data: data);
+      final responseData = jsonDecode(response.body);
+
+      print("Response dari API: $responseData"); // Debugging
+
+      if (response.statusCode == 200 && responseData.containsKey('token')) {
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        await preferences.setInt('user_id', responseData['user']['id']);
+        await preferences.setString(
+            'nik', responseData['user']['masyarakat']['nik']);
+        await preferences.setString('token', responseData['token']);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(responseData['message'])),
+        );
+
+        print("Navigasi ke Dashboard...");
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => DashboardPage()),
+        );
+      } else {
+        print("Login gagal: ${responseData['message']}");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Login gagal: ${responseData['message']}')),
+        );
+      }
+    } catch (e) {
+      print("Terjadi kesalahan: $e");
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(response['message']),
-        ),
-      );
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => DashboardPage(),
-        ),
+        SnackBar(content: Text('Terjadi kesalahan: ${e.toString()}')),
       );
     }
   }
 
-  // Future<void> login() async {
-  //   if (!_formSignInKey.currentState!.validate()) return;
-
-  //   final authProvider = Provider.of<AuthProvider>(context, listen: false);
-  //   bool isSuccess =
-  //       await authProvider.login(nikController.text, passwordController.text);
-
-  //   if (isSuccess) {
-  //     SharedPreferences prefs = await SharedPreferences.getInstance();
-  //     await prefs.setString('token', authProvider.user!.token!);
-
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(content: Text('Login Berhasil')),
-  //     );
-
-  //     Navigator.pushReplacement(
-  //       context,
-  //       MaterialPageRoute(builder: (context) => DashboardPage()),
-  //     );
-  //   } else {
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text(authProvider.errorMessage ?? 'Login gagal')),
-  //     );
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<AuthProvider>(context);
-
     return CustomScaffold(
       child: Column(
         children: [
@@ -194,34 +179,14 @@ class _LoginState extends State<Login> {
                         ],
                       ),
                       const SizedBox(height: 20.0),
-                      SizedBox(
-                        width: double.infinity,
-                        child: authProvider.isLoading
-                            ? const Center(child: CircularProgressIndicator())
-                            : InkWell(
-                                onTap: () {
-                                  loginUser();
-                                },
-                                child: Container(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 12),
-                                  decoration: BoxDecoration(
-                                    color: Colors
-                                        .blue, // Ganti dengan warna yang diinginkan
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: const Center(
-                                    child: Text(
-                                      "Login",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print("Tombol login ditekan!");
+                          if (_formSignInKey.currentState!.validate()) {
+                            loginUser();
+                          }
+                        },
+                        child: Text("Login"),
                       ),
                       const SizedBox(height: 20.0),
                       Row(
